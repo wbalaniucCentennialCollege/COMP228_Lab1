b54fc547eaf0bf16804772a484401c0071dadcb3
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2016-09-09 20:10:19 -0400

    Initial commit
    I don't want to type in a description

---
+++ Welcome1.java
@@ -0,0 +1,10 @@
+// This is class Welcome!                                                                                             
+public class Welcome1
+{
+	// Main entry point for the application
+	public static void main(String[] args)
+	{
+		// Display a welcome message to the console
+		System.out.printf("%s%n%s", "Welcome to ", "Java Programming");
+	}	
+}
